4ba9eaa28f6d033d1dcb31949372c5e631cabdae
MorgantDest <110782029+MorgantDest@users.noreply.github.com>
2024-10-26 10:42:14 +0300

    Update twrp_X96Pro.mk

--- twrp_X96Pro.mk
+++ twrp_X96Pro.mk
@@ -1,28 +1,23 @@
-#
 # Copyright (C) 2024 The Android Open Source Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from X96Pro device
 $(call inherit-product, device/doogee/X96Pro/device.mk)
 
 PRODUCT_DEVICE := X96Pro
-PRODUCT_NAME := X96Pro
+PRODUCT_NAME := twrp_X96Pro
 PRODUCT_BRAND := DOOGEE
 PRODUCT_MODEL := X96Pro
 PRODUCT_MANUFACTURER := doogee
-
-PRODUCT_GMS_CLIENTID_BASE := android-doogee
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="DOOGEE-X96Pro_RU_20210429_V02"
-
-BUILD_FINGERPRINT := DOOGEE/X96Pro_RU/X96Pro:11/RP1A.201005.001/17435:user/release-keys
